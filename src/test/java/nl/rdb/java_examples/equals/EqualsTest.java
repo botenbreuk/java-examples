52c07e3f45688bcfd2fd8c020e6db145f4a93157
Robin de Boer <robin.de.boer@42.nl>
2022-04-12 08:18:13 +0200

    Added tests for equals test on strings

--- src/test/java/nl/rdb/java_examples/equals/EqualsTest.java
+++ src/test/java/nl/rdb/java_examples/equals/EqualsTest.java
@@ -1,6 +1,8 @@
 package nl.rdb.java_examples.equals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class EqualsTest {
@@ -48,6 +51,43 @@ class EqualsTest {
         public TestListObject(String name, int age) {
             this.name = name;
             this.age = age;
+        }
+    }
+
+    @Nested
+    class EqualsStringTest {
+
+        @Test
+        void testStringEquals() {
+            String test = "test";
+            assertTrue(test.equals("test"));
+            assertTrue(test == "test");
+
+            String test2 = "test";
+            assertTrue(test.equals(test2));
+            assertTrue(test == test2);
+
+            test = test2;
+            assertTrue(test.equals(test2));
+            assertTrue(test == test2);
+
+            String test3 = test;
+            assertTrue(test.equals(test2));
+            assertTrue(test.equals(test3));
+            assertTrue(test == test2);
+            assertTrue(test == test3);
+        }
+
+        // Test based on example in https://www.delftstack.com/howto/java/java-string-equals-vs-/
+        @Test
+        void testStringEquals_withStringConstructor() {
+            String str1 = "name";
+            String str2 = "name";
+            String str3 = new String("name");
+            assertTrue(str1.equals(str2));
+            assertTrue(str1 == str2);
+            assertTrue(str1.equals(str3));
+            assertFalse(str3 == str1);
         }
     }
 }
