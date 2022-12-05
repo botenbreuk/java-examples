e58bb87036d610141afecc135714a63015518cc7
Robin de Boer <robin.de.boer@42.nl>
2022-12-05 14:40:18 +0100

    Updated the TimeLogger.java

--- src/main/java/nl/rdb/java_examples/utils/logging/TimeLogger.java
+++ src/main/java/nl/rdb/java_examples/utils/logging/TimeLogger.java
@@ -3,7 +3,9 @@ package nl.rdb.java_examples.utils.logging;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -28,8 +30,8 @@ public class TimeLogger {
     }
 
     public static void logTimeFinish(String message, Runnable function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        logTimeFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), function);
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        logTimeFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), function);
     }
 
     public static <T> T logTimeFinish(String name, String message, Supplier<T> function) {
@@ -41,8 +43,8 @@ public class TimeLogger {
     }
 
     public static <T> T logTimeFinish(String message, Supplier<T> function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        return logTimeFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), function);
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        return logTimeFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), function);
     }
 
     public static <T> T logTimeStartFinish(String name, String message, Supplier<T> function) {
@@ -55,8 +57,8 @@ public class TimeLogger {
     }
 
     public static <T> T logTimeStartFinish(String message, Supplier<T> function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        return logTimeStartFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), function);
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        return logTimeStartFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), function);
     }
 
     public static void logTimeStartFinish(String name, String message, Runnable function) {
@@ -67,9 +69,37 @@ public class TimeLogger {
         log.info("{} > Finished: {} took {}ms", name, message, stopWatch.getTime());
     }
 
+    public static <T> T logTimeStartFinishUn(String name, String message, T obj, UnaryOperator<T> function) {
+        StopWatch stopWatch = StopWatch.createStarted();
+        log.info("{} > Started: {}", name, message);
+        T result = function.apply(obj);
+        stopWatch.stop();
+        log.info("{} > Finished: {} took {}ms", name, message, stopWatch.getTime());
+        return result;
+    }
+
+    public static <T> T logTimeStartFinishUn(String message, T obj, UnaryOperator<T> function) {
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        return logTimeStartFinishUn(calledBy.getShortClass(), message, obj, function);
+    }
+
+    public static <T, R> R logTimeStartFinishFn(String name, String message, T obj, Function<T, R> function) {
+        StopWatch stopWatch = StopWatch.createStarted();
+        log.info("{} > Started: {}", name, message);
+        R result = function.apply(obj);
+        stopWatch.stop();
+        log.info("{} > Finished: {} took {}ms", name, message, stopWatch.getTime());
+        return result;
+    }
+
+    public static <T, R> R logTimeStartFinishFn(String message, T obj, Function<T, R> function) {
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        return logTimeStartFinishFn(calledBy.getShortClass(), message, obj, function);
+    }
+
     public static void logTimeStartFinish(String message, Runnable function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        logTimeStartFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), function);
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        logTimeStartFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), function);
     }
 
     public static <T> void logTimeStartFinish(String name, String message, List<List<T>> partitions, Consumer<List<T>> function) {
@@ -85,8 +115,8 @@ public class TimeLogger {
     }
 
     public static <T> void logTimeStartFinish(String message, List<List<T>> partitions, Consumer<List<T>> function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        logTimeStartFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), partitions, function);
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        logTimeStartFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), partitions, function);
     }
 
     public static void logStartFinish(String name, String message, Runnable function) {
@@ -95,25 +125,37 @@ public class TimeLogger {
         log.info("{} > Finished: {}", name, message);
     }
 
-    public static void logStartFinish(String message, Runnable function) {
-        StackTraceRecord traceRecord = new StackTraceRecord();
-        logStartFinish(traceRecord.getShowrtClasse(), METHOD_STRING.formatted(traceRecord.getClassMethod(), message), function);
+    public static <T> T logStartFinish(String message, T obj, UnaryOperator<T> function) {
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        log.info("{} > Started: {}", calledBy.getShortClass(), message);
+        T result = function.apply(obj);
+        log.info("{} > Finished: {}", calledBy.getShortClass(), message);
+        return result;
     }
 
-    private static final class StackTraceRecord {
+    public static void logStartFinish(String message, Runnable function) {
+        CalledByClassMethod calledBy = new CalledByClassMethod();
+        logStartFinish(calledBy.getShortClass(), calledBy.getClassMethod(message), function);
+    }
+
+    private static final class CalledByClassMethod {
         private final String className;
         private final String methodName;
 
-        private StackTraceRecord() {
+        private CalledByClassMethod() {
             StackTraceElement trace = Arrays.stream(new Throwable().getStackTrace()).limit(5).toList().get(2);
             this.className = trace.getClassName();
             this.methodName = trace.getMethodName();
         }
 
-        public String getShowrtClasse() {return Arrays.stream(className.split("\\.")).reduce((first, second) -> second).orElse("");}
+        public String getShortClass() {return Arrays.stream(className.split("\\.")).reduce((first, second) -> second).orElse("");}
 
         public String getClassMethod() {
-            return "%s.%s()".formatted(this.className, this.methodName);
+            return "%s::%s()".formatted(getShortClass(), this.methodName);
+        }
+
+        public String getClassMethod(String message) {
+            return METHOD_STRING.formatted(getClassMethod(), message);
         }
     }
 }
--- src/main/java/nl/rdb/java_examples/utils/logging/TimeLoggerExample.java
+++ src/main/java/nl/rdb/java_examples/utils/logging/TimeLoggerExample.java
@@ -1,13 +1,87 @@
 package nl.rdb.java_examples.utils.logging;
 
+import java.util.List;
+
 import lombok.extern.slf4j.Slf4j;
 import nl.rdb.java_examples.scanner.Example;
+
+import com.google.common.collect.Lists;
 
 @Slf4j
 public class TimeLoggerExample {
 
+    @Example(name = "TimeLogger with Name as parameter")
+    public void timeLoggerWithName() {
+        String name = getClass().getSimpleName();
+
+        TimeLogger.logTimeFinish(name, "Testing the Timelogger only finish", () -> log.info("I have meen called"));
+
+        String hello = TimeLogger.logTimeFinish(name, "Testing the Timelogger finish with return", () -> "Returned Hello world!");
+        log.info("Message: {}", hello);
+
+        String helloWorld = TimeLogger.logTimeStartFinish(name, "Testing the Timelogger start and finish with return", () -> "Hello world TWO!");
+        log.info("Message: {}", helloWorld);
+
+        TimeLogger.logTimeStartFinish(name, "Testing the Timelogger start and finish", () -> log.info("I have meen called"));
+
+        List<String> testVar = Lists.newArrayList("Hello", "World");
+        List<String> testingVar = TimeLogger.logTimeStartFinishUn(name, "Testing return value", testVar, list -> {
+            list.add("!");
+            return list;
+        });
+        log.info("Message: {}", String.join(" ", testingVar));
+
+        List<String> testVar2 = Lists.newArrayList("Hello", "World");
+        String testingVar2 = TimeLogger.logTimeStartFinishFn(name, "Testing return value", testVar2, list -> {
+            list.add("!");
+            return String.join(" ", list);
+        });
+        log.info("Message: {}", testingVar2);
+
+        List<String> first = Lists.newArrayList("Hallo", "Wereld", "!");
+        List<String> second = Lists.newArrayList("Hello", "World", "!");
+        List<List<String>> parts = Lists.newArrayList(first, second);
+        TimeLogger.logTimeStartFinish(name, "Testing partitions", parts, part -> log.info("Joining parts: {}", String.join(" ", part)));
+
+        TimeLogger.logStartFinish(name, "Testing timelogger with sleep", () -> log.info("Start finish"));
+    }
+
     @Example
     public void timeLogger() {
-        TimeLogger.logTimeFinish("Testing the timelogger", () -> log.info("I have meen called"));
+        TimeLogger.logTimeFinish("Testing the Timelogger only finish", () -> log.info("I have meen called"));
+
+        String hello = TimeLogger.logTimeFinish("Testing the Timelogger finish with return", () -> "Returned Hello world!");
+        log.info("Message: {}", hello);
+
+        String helloWorld = TimeLogger.logTimeStartFinish("Testing the Timelogger start and finish with return", () -> "Hello world TWO!");
+        log.info("Message: {}", helloWorld);
+
+        TimeLogger.logTimeStartFinish("Testing the Timelogger start and finish", () -> log.info("I have meen called"));
+
+        List<String> testVar = Lists.newArrayList("Hello", "World");
+        List<String> testingVar = TimeLogger.logTimeStartFinishUn("Testing return value", testVar, list -> {
+            list.add("!");
+            return list;
+        });
+        log.info("Message: {}", String.join(" ", testingVar));
+
+        List<TestString> listHW = Lists.newArrayList(
+                new TestString("Hello"),
+                new TestString("World")
+        );
+        String testingVar2 = TimeLogger.logTimeStartFinishFn("Testing return different type value", listHW, testList -> {
+            testList.add(new TestString("!"));
+            return String.join(" ", testList.stream().map(TestString::label).toList());
+        });
+        log.info("Message: {}", testingVar2);
+
+        List<String> first = Lists.newArrayList("Hallo", "Wereld", "!");
+        List<String> second = Lists.newArrayList("Hello", "World", "!");
+        List<List<String>> parts = Lists.newArrayList(first, second);
+        TimeLogger.logTimeStartFinish("Testing partitions", parts, part -> log.info("Joining parts: {}", String.join(" ", part)));
+
+        TimeLogger.logStartFinish("Testing timelogger with sleep", () -> log.info("Start finish"));
     }
+
+    private record TestString(String label) {}
 }
