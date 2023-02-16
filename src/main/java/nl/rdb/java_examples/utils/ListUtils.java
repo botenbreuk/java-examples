58892c25f0f525ef78a5ce2ad058a5586340a458
Robin de Boer <robin.de.boer@42.nl>
2023-02-16 11:01:48 +0100

    Updated the ListUtils

--- src/main/java/nl/rdb/java_examples/utils/ListUtils.java
+++ src/main/java/nl/rdb/java_examples/utils/ListUtils.java
@@ -3,9 +3,26 @@ package nl.rdb.java_examples.utils;
 import java.util.List;
 import java.util.function.ObjIntConsumer;
 
-public class ListUtils {
+public class ListUtils<T> {
 
-    private ListUtils() {throw new IllegalStateException("Utility class");}
+    private final List<T> list;
+
+    private ListUtils(List<T> list1) {this.list = list1;}
+
+    public static <T> ListUtils<T> of(List<T> list) {
+        return new ListUtils<>(list);
+    }
+
+    public void forEach(ObjIntConsumer<T> fn) {
+        if (list == null || list.isEmpty()) {
+            return;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            T value = list.get(i);
+            fn.accept(value, i);
+        }
+    }
 
     public static <T> void forEach(List<T> collection, ObjIntConsumer<T> fn) {
         if (collection == null || collection.isEmpty()) {
