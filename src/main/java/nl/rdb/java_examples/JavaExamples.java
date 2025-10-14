df444389db54deb899de0c6ed1f1ddbbc58861c0
Robin de Boer <robin.de.boer@42.nl>
2025-10-14 11:09:42 +0200

    Updated to Java 25

--- src/main/java/nl/rdb/java_examples/JavaExamples.java
+++ src/main/java/nl/rdb/java_examples/JavaExamples.java
@@ -1,12 +1,10 @@
 package nl.rdb.java_examples;
 
-import lombok.extern.slf4j.Slf4j;
 import nl.rdb.java_examples.scanner.ExampleScanner;
 
-@Slf4j
 public class JavaExamples {
 
-    public static void main(String[] args) {
+    static void main() {
         ExampleScanner.executeExamples();
     }
 }
