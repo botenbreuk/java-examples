caf450145dce30fa78af9b8650faf0bf113eca0a
Robin de Boer <robin.de.boer@42.nl>
2026-06-15 09:13:07 +0200

    Added a null check example for enums

--- src/main/java/nl/rdb/java_examples/enums/EnumExample.java
+++ src/main/java/nl/rdb/java_examples/enums/EnumExample.java
@@ -11,4 +11,13 @@ public class EnumExample {
         log.info(TestEnum.HELLO.name());
         log.info("{}", Authority.CLIENTS_MANAGE);
     }
+
+    @Example
+    void nullExample() {
+        TestEnum nullValue = null;
+        TestEnum notNullValue = TestEnum.HELLO;
+
+        log.info("Null value check with ==: {}", nullValue == TestEnum.HELLO);
+        log.info("Not null value check with ==: {}", notNullValue == TestEnum.HELLO);
+    }
 }
